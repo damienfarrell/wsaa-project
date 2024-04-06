505b0bc03637ac4b243f8ad4de1af564cc0c490a
damienfarrell <damien.farrell1@gmail.com>
2024-04-06 21:56:32 +0100

    Added SQL statements

--- app/main.py
+++ app/main.py
@@ -9,15 +9,16 @@ import time
 app = FastAPI()
 
 class Film(BaseModel):
-    id: int
+    # id: int
     title: str
-    watched: Union[bool, None] = None
-    review: Union[int, None] = None
+    is_watched: Union[bool, None] = None
+    rating: Union[int, None] = None
     comment: Union[str, None] = None
+
 
 while True:
     try:
-        conn = psycopg2.connect()
+
         cursor = conn.cursor()
         print("Database connection was successful")
         break
@@ -36,19 +37,37 @@ def read_films():
     films = cursor.fetchall()
     return {"data": films}
 
+@app.post("/films", status_code=status.HTTP_201_CREATED)
+def create_films(film: Film):
+    cursor.execute("""INSERT INTO films (title) VALUES (%s) RETURNING *""", (film.title,))
+    new_film = cursor.fetchone()
+    conn.commit()
+    return {"data": new_film}
+
 @app.get("/films/{id}")
 def read_film(id: int):
-    return {"film_id": id, "message": "Film data goes here"}
+    cursor.execute("""SELECT * FROM films WHERE id = %s""", (id,))
+    film = cursor.fetchone()
+    if not film:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"film with id: {id} was not found")
+    return {"data" : film}
 
-@app.post("/films/{id}")
-def create_film(film: Film):
-    return {"film_id": film.id, "film_name": film}
+@app.delete("/films/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_film(id: int):
+    cursor.execute("""DELETE FROM films WHERE id = %s RETURNING *""", (id,))
+    deleted_film = cursor.fetchone()
+    conn.commit()
+    if deleted_film == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+#### TODO I need to change and update the SQL statments on POST and PUT
 
 @app.put("/films/{id}")
-def update_film(film: Film):
-    return {"film_id": film.id, "film_name": film}
-
-@app.delete("/films/{id}")
-def delete_film(film: Film):
-    return {"film_id": film.id, "film_name": film}
-
+def update_film(id: int, film: Film):
+    cursor.execute("""UPDATE films SET title = %s WHERE id = %s RETURNING *""", (film.title, id))
+    updated_film = cursor.fetchone()
+    conn.commit()
+    if updated_film == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    return {"data" : updated_film}
